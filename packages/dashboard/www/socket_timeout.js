6c5c877819ecb11313f3790915af8f21f8b4b2ea
izzy-everall <izzy.everall@gmail.com>
2021-01-11 18:20:15 +0000

    add .js file

---
+++ packages/dashboard/www/socket_timeout.js
@@ -0,0 +1,12 @@
+var socket_timeout_interval;
+var n = 0;
+
+$(document).on('shiny:connected', function(event) {
+  socket_timeout_interval = setInterval(function() {
+    Shiny.onInputChange('alive_count', n++)
+  }, 10000);
+});
+
+$(document).on('shiny:disconnected', function(event) {
+  clearInterval(socket_timeout_interval)
+});
