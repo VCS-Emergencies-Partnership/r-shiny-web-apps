2c364a8ab67c4d40c753a8de559de0c012e3f710
izzy-everall <izzy.everall@gmail.com>
2021-06-14 12:22:34 +0100

    add public resources script

---
+++ packages/dashboard/preprocess_scripts/prep-public-resources.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+library(feather)
+
+
+resources <- read_csv("~/vcs-indicators/resource_bank_8june.csv")
+
+resources$`Relevance/Include?`
+
+wanted <- c("YES","Yes", "yes")
+# filter out no
+to_include <- resources %>% filter(`Relevance/Include?` %in% wanted)
+
+
+# write to feather
+write_feather(to_include, "~/r-shiny-web-apps/packages/dashboard/data/resource_bank/resource_bank.feather")
